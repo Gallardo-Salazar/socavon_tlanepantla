b65eab20d72d5613eaa559cd5ad0627e475352b6
unknown <https://github.com/Gallardo-Salazar/socavon_tlanepantla.git>
2023-09-01 12:57:45 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 63487077,
+    "boundingBox": {
+        "lx": 487984.28125,
+        "ly": 2158538.25,
+        "lz": 2232.4951171875,
+        "ux": 488181.28125,
+        "uy": 2158735.25,
+        "uz": 2429.4951171875
+    },
+    "tightBoundingBox": {
+        "lx": 487984.274375,
+        "ly": 2158538.2229,
+        "lz": 2232.4950654296877,
+        "ux": 488119.563175,
+        "uy": 2158735.3727,
+        "uz": 2275.9890654296873
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.706070065498352,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
